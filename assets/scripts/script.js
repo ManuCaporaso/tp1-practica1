3836428c358cdf1bc5e19358013d60109efe56da
Manu Caporaso <manulpda@gmail.com>
2024-09-09 23:32:34 -0300

    cambios finales

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,8 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const contactForm = document.getElementById('contact-form');
     contactForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Evita el envío del formulario por defecto
-
-        // Aquí podrías agregar código para enviar el formulario usando Fetch API o XMLHttpRequest
         alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
         
         contactForm.reset(); // Reinicia el formulario después de enviar
